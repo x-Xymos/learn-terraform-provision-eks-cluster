bb95b8c74fd90ae0b32335e123bddfb90fdbff92
Xymos <xymos0@gmail.com>
2022-10-29 18:56:11 +0100

    .

--- main.tf
+++ main.tf
@@ -31,11 +31,11 @@ terraform {
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
+    host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
       command     = "aws"
     }
   }
