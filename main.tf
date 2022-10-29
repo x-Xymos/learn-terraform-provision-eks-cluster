68b7245153e78fdb00c0118d524a23ec272921c8
Xymos <xymos0@gmail.com>
2022-10-29 21:32:47 +0100

    .

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-}
+
 
 provider "aws" {
   region = var.region
@@ -32,6 +29,18 @@ data "aws_eks_cluster_auth" "cluster_auth" {
   name = local.cluster_name
 }
 
+provider "kubernetes" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token = data.aws_eks_cluster_auth.cluster_auth.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+      command     = "aws"
+    }
+  }
+}
 
 provider "helm" {
   kubernetes {
